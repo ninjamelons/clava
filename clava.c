4e52a1ef9981809e7e20b5067366b4bd6266ff3f
Filip Olsen <filip.kenya@gmail.com>
2022-04-05 19:48:18 +0200

    Changed pointer array to [], demo gif file

--- clava.c
+++ clava.c
@@ -13,9 +13,12 @@
 struct metaball {
 	int y, x, radius;
 	float vx, vy;
+	int isMoveable;
+	float moveSpeed;
 };
 
-struct metaball *new_metaball(int, int, short);
+struct metaball *new_metaball(int, int, int);
+struct metaball *new_metaball_mv(int, int, int, int, float);
 void free_metaball(struct metaball*);
 float Q_rsqrt(float);
 
@@ -27,6 +30,7 @@ int main() {
 	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
 	noecho();					/* Don't echo() while we do getch */
 	curs_set(0);
+	srand(time(NULL));
 
 	int row, col;
 	getmaxyx(wnd, row, col);	/* Get window dimensions */
@@ -38,18 +42,33 @@ int main() {
 	char msg[] = "Totally a lava lamp";
 	mvwprintw(wnd, 1, (col-strlen(msg))/2, "%s", msg); /* move to x,y, print to w */
 
-	struct metaball *ball = new_metaball(15, 15, 5);
-	struct metaball *ball1 = new_metaball(15, 30, 5);
-
-	size_t size = 2;
-	float threshold = 1.5f;
+	size_t size = 16;
+	float threshold = 1.8f;
 	int factor = 1;
-	struct metaball *vec = malloc(size * sizeof(struct metaball));
-	*vec = *ball;
-	*(vec + sizeof(vec)) = *ball1;  
+	struct metaball *vec[] = {
+		// Floating blobs
+		new_metaball(15, 15, 10),
+		new_metaball(15, 45, 5),
+		// Immoveable floor
+		new_metaball_mv(row, col / 2, row / 3.0f, 0, 0.0f),
+		new_metaball_mv(row, col / 2 + 10, row / 3.0f, 0, 0.0f),
+		new_metaball_mv(row, col / 2 + 20, row / 3.0f, 0, 0.0f),
+		new_metaball_mv(row + 1, col / 2 - 10, row / 3.0f, 0, 0.0f),
+		new_metaball_mv(row + 1, col / 2 - 20, row / 3.0f, 0, 0.0f),
+		new_metaball_mv(row + 1, col / 2 + 33, row / 3.0f, 0, 0.0f),
+		new_metaball_mv(row + 1, col / 2 - 33, row / 3.0f, 0, 0.0f),
+		// Immoveable Ceiling
+		new_metaball_mv(0 - 1, col / 2, row / 3.5f, 0, 0.0f),
+		new_metaball_mv(0 - 1, col / 2 + 10, row / 3.5f, 0, 0.0f),
+		new_metaball_mv(0 - 1, col / 2 - 10, row / 3.5f, 0, 0.0f),
+		new_metaball_mv(0 - 2, col / 2 + 20, row / 4.0f, 0, 0.0f),
+		new_metaball_mv(0 - 2, col / 2 - 20, row / 4.0f, 0, 0.0f),
+		new_metaball_mv(0 - 2, col / 2 + 25, row / 4.0f, 0, 0.0f),
+		new_metaball_mv(0 - 2, col / 2 - 25, row / 4.0f, 0, 0.0f),
+	};
 
 	int frameCount = 0;
-	int frameTime = 60; // Time in milliseconds
+	int frameTime = 180; // Time in milliseconds
 	int isQuit = 0;
 	while(!isQuit) {
 		// Clear window for next run
@@ -68,12 +87,13 @@ int main() {
 		} while (res && errno == EINTR);
 
 		// Draw to screen
-		for(int x = 0; x < col; x++) {
-			for(int y = 0; y < row; y++) {
+		for(int x = 0; x <= col; x++) {
+			for(int y = 0; y <= row; y++) {
 				float falloff_sum = 0;
 				int counter = 0;
 
-				for(struct metaball *v = vec; v < vec + size*sizeof(vec); v += sizeof(vec)) {
+				for(int i = 0; i < size; i++) {
+					struct metaball *v = vec[i];
 					float dx = (float)x - v->x;
 					float dy = ((float)y - v->y) * 2; // Font is taller than it is wide
 					float dr = ((float)v->radius * v->radius);
@@ -86,12 +106,7 @@ int main() {
 				// printf("counter: %d;", counter); 
 
 				if(falloff_sum > threshold) {
-					// Check if within bounds
-					if(y < row && y > 0 &&
-							x < col && x > 0) {
-						// Draw point
-						mvwprintw(wnd, y, x, "%s", PIXEL);
-					}
+					mvwprintw(wnd, y, x, "%s", PIXEL);
 				}
 			}
 		}
@@ -100,60 +115,60 @@ int main() {
 		// Move balls
 		// Random slow horizontal movement
 		// Bouyant from bottom to top, with pauses in between
-		for(struct metaball *v = vec; v < vec + size*sizeof(vec); v += sizeof(vec)) {	
-			// Only move X points per frame
-			
-			// Normalize X,Y
-			float norm_x = (float)v->x / col;
-			float norm_y = (float)v->y / row;
-			// Between -0.5 and 0.5
-			float norm_signed_x = norm_x - 0.5f;
-			float norm_signed_y = norm_y - 0.5f;
+		for(int i = 0; i < size; i++) {
+			struct metaball *v = vec[i];
+			if(v->isMoveable) {
 
-			// Seed rand and get random scaling factor for displacement
-			srand(time(NULL));
-			float factor_x = (float)rand() / RAND_MAX * 0.5;
-			float factor_y = (float)rand() / RAND_MAX * 0.5;
+				// Normalize X,Y
+				float norm_x = (float)v->x / col;
+				float norm_y = (float)v->y / row;
+				// Between negative and positive value
+				float norm_signed_x = norm_x - 0.5f;
+				float norm_signed_y = norm_y * 3 - 1.5f;
 
-			v->vx += -1 * norm_signed_x * factor_x;
-			v->vy += -1 * norm_signed_y * factor_y;
+				// Seed rand and get random scaling factor for displacement
+				float factor_x = (float)rand() / RAND_MAX * 0.2;
+				float factor_y = (float)rand() / RAND_MAX;
 
-			v->x += v->vx;
-			v->y += v->vy;
-			
+				v->vx += -1 * norm_signed_x * factor_x * v->moveSpeed;
+				v->vy += -1 * norm_signed_y * factor_y * v->moveSpeed;
+
+				v->x += v->vx;
+				v->y += v->vy;
+
+				if(v->x > col)
+					v->x = col;
+				else if(v->x < 0)
+					v->x = 0;
+				if(v->y > row)
+					v->y = row;
+				else if(v->y < 0)
+					v->y = 0;
+			}
 			/*
-			if(v->x > col)
-				v->x = col;
-			else if(v->x < 0)
-				v->x = 0;
-			if(v->y > row)
-				v->y = row;
-			else if(v->y < 0)
-				v->y = 0;
-			*/
-
 			counter++;
 			mvwprintw(wnd, row - counter, 0, "VX: %f, VY: %f, X: %d, Y: %d"
-					, v->vx, v->vy, v->x, v->y); 
+					, v->vx, v->vy, v->x, v->y);
+			*/
 		}
 
-		mvwprintw(wnd, row-5, 1, "Frame: %d", frameCount);
+		// mvwprintw(wnd, row-5, 1, "Frame: %d", frameCount);
 		frameCount++;
 		wrefresh(wnd);
 	}
 
 	wattroff(wnd, COLOR_PAIR(wax_color));
-
-	free_metaball(ball);
-	free_metaball(ball1);
-	free(vec);
+	
+	for(int i = 0; i < size; i++) {
+		free_metaball(vec[i]);
+	}
 
 	endwin();
 
 	return 0;
 }
 
-struct metaball *new_metaball(int y, int x, short radius) {
+struct metaball *new_metaball_mv(int y, int x, int radius, int isMoveable, float moveSpeed) {
 	struct metaball *ball = malloc(sizeof(struct metaball));
 	ball->y = y;
 	ball->x = x;
@@ -161,8 +176,16 @@ struct metaball *new_metaball(int y, int x, short radius) {
 
 	ball->vx = 0.0f;
 	ball->vy = 0.0f;
+
+	ball->isMoveable = isMoveable;
+	ball->moveSpeed = moveSpeed;
+
 	return ball;
 }
+struct metaball *new_metaball(int y, int x, int radius) {
+	return new_metaball_mv(y, x, radius, 1, 1.0f);
+}
+
 void free_metaball(struct metaball *ball) {
 	if(ball != NULL) {
 		free(ball);
